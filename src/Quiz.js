e224477aba024e9425726d84741fb1552caec340
Victor Gutemberg Oliveira Marques <violivei@microsoft.com>
2018-06-21 13:45:59 -0700

    Changing comparison order for isQuizEnd

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     this.state = { quiz_position: 1}
   }
   render() {
-    const isQuizEnd = quizData.quiz_questions.length == (this.state.quiz_position - 1);
+    const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length;
 
     return (
       <div>
