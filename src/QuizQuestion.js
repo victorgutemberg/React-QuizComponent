d21b866b6692a4ebf2d281cd578186a81b89be15
Victor Gutemberg Oliveira Marques <violivei@microsoft.com>
2018-06-21 11:39:28 -0700

    Adding className to buttons div

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,9 +5,9 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p> {this.props.quiz_question.instruction_text} </p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
-        <section>
+        <section className="buttons">
           <ul>
            <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
