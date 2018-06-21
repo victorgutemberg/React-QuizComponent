cdcf9e97256864c93a8058ff903601d6df18fdf8
Victor Gutemberg Oliveira Marques <violivei@microsoft.com>
2018-06-21 14:25:34 -0700

    using ternary conditional

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,8 +20,7 @@ class Quiz extends Component {
 
     return (
       <div>
-        {isQuizEnd && <QuizEnd />}
-        {!isQuizEnd && <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
+        {isQuizEnd? <QuizEnd />: <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
       </div>
     );
   }
